7803eacbf323ceeca2bc16a1d85a401829e58247
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-05-09 08:39:23 +0900

    success to generate color gradation

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,21 @@ int main(int argc, char const *argv[])
 
     std::cout << "P3\n"
               << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n"
-              << "255\n"
-              << std::endl;
+              << "255\n";
 
-    for (int i = 0; i < IMAGE_HEIGHT - 1, i >= 0; i++)
+    for (int j = IMAGE_HEIGHT - 1; j >= 0; --j)
     {
-        for (int j = 0; j < IMAGE_WIDTH - 1; j++)
+        for (int i = 0; i < IMAGE_WIDTH; ++i)
         {
-            auto r = double(i) / (IMAGE_WIDTH - 1);
-            auto g = double(j) / (IMAGE_HEIGHT - 1);
-            auto b = 0.25;
+            double r = double(i) / (IMAGE_WIDTH - 1);
+            double g = double(j) / (IMAGE_HEIGHT - 1);
+            double b = 0.25;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
+            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
         }
     }
 
