d08d2690d9f5f1bb2579ebca1c1b3c64da743db7
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-05-12 21:55:49 +0000

    add hittable list class

---
+++ src/hittable_list.h
@@ -0,0 +1,23 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+class HittableList : public : Hittable
+{
+private:
+    /* data */
+public:
+    HittableList();
+    ~HittableList();
+};
+
+HittableList::HittableList(/* args */)
+{
+}
+
+HittableList::~HittableList()
+{
+}
+
+#endif
