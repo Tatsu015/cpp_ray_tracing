262b518883824456b9c58d6b6694fdf316c48a02
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-05-14 00:14:11 +0000

    fix comple error

--- src/hittable_lists.cpp
+++ src/hittable_lists.cpp
@@ -19,15 +19,16 @@ void HittableList::clear()
     objects_.clear();
 }
 
-bool Hittable::hit(const Ray &r, const double t_min, const double t_max, HitRecord &rec) const
+bool HittableList::hit(const Ray &r, const double t_min, const double t_max, HitRecord &rec) const
 {
+
     HitRecord tmp_rec;
     bool hit_anything = false;
     double closest_so_far = t_max;
 
-    for (auto o : objets_)
+    for (auto o : objects_)
     {
-        if (o.hit(r, t_min, closest_so_far, tmp_rec))
+        if (o->hit(r, t_min, closest_so_far, tmp_rec))
         {
             hit_anything = true;
             closest_so_far = tmp_rec.t;
